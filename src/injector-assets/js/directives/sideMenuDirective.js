25cfe13b08ee5a26a10d71675cb6b0c11f1b9b2e
Juan López Rubio <j.lopez.r@gmail.com>
2016-11-14 13:19:31 +0100

    Cleaning directive side menu

--- src/injector-assets/js/directives/sideMenuDirective.js
+++ src/injector-assets/js/directives/sideMenuDirective.js
@@ -18,16 +18,13 @@
                     $scope.isOpen = false;
 
                     var render = function () {
-                        console.log("in render");
                         $scope.sections = new Sections();
 
-                        //$scope.customMenu = [];
                         angular.forEach(customMenu, function (elem) {
                             $scope.sections.add(elem.section, elem.title, elem);
                         });
 
                         models.getModels(function (m) {
-                            console.log("in callback");
                             angular.forEach(m, function (schema) {
                                 models.getModelConfig(schema, function (config) {
                                     if (config.isSingle) {
@@ -41,9 +38,6 @@
                                     } else {
                                         config.clickTo = "model/" + schema;
                                     }
-
-                                    //$scope.sections[config.section] = $scope.sections[config.section] || {};
-                                    //$scope.sections[config.section][schema] = config;
                                     $scope.sections.add(config.section, schema, config);
                                 });
                             });
@@ -53,7 +47,6 @@
 
                     render();
                     $rootScope.$on('invalidate', function () {
-                        console.log("invalidate side menu");
                         render();
                     });
 
@@ -62,7 +55,6 @@
                     };
 
                     $scope.click = function (section, name, conf) {
-                        //console.log(section);
                         $scope.parentSchema = section;
                         $scope.actualSchema = name;
                         $scope.actualSection = section;
@@ -72,14 +64,6 @@
                             $window.location.href = conf.url;
                         }
                     };
-
-                    ////$scope.customMenu = [];
-                    //angular.forEach(customMenu, function (elem) {
-                    //    $scope.sections[elem.section] = $scope.sections[elem.section] || {};
-                    //    $scope.sections[elem.section][elem.name] = {};
-                    //    $scope.sections[elem.section][elem.name].clickTo = elem.url;
-                    //});
-
 
                     $scope.isInstanceOf = function (obj) {
                         return (obj instanceof Section);
@@ -125,15 +109,3 @@ var Sections = function () {
         curLevel[levels[levels.length - 1]][schema] = config;
     };
 };
-
-//var sections = new Sections();
-//sections.add('accordeon', 'model1', 'config1');
-//sections.add('accordeon', 'model2', 'config2');
-//sections.add('accordeon2', 'model3', 'config3');
-//sections.add('accordeon.subaccordeaon', 'model4', 'config4');
-//sections.add('accordeon.subaccordeaon2', 'model5', 'config5');
-//sections.add('accordeon.subaccordeaon2', 'model6', 'config6');
-//var a = sections.get();
-//console.log(a);
-//
-//console.log(a.accordeon instanceof Section);
