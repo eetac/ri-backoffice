6b38e659a3f9360a968d10cb2de3f8e18a98c4bc
Alex Baena <a.baena@ondho.com>
2017-02-07 18:55:43 +0100

    Sorted menu sections

--- src/injector-assets/js/directives/sideMenuDirective.js
+++ src/injector-assets/js/directives/sideMenuDirective.js
@@ -92,7 +92,7 @@ var Sections = function () {
     var menu = {};
 
     this.get = function () {
-        return menu;
+        return orderKeys(menu);
     };
 
 
@@ -116,3 +116,22 @@ var Sections = function () {
         curLevel[levels[levels.length - 1]][schema] = config;
     };
 };
+function orderKeys(obj) {
+
+    var keys = Object.keys(obj).sort(function keyOrder(k1, k2) {
+        if (k1 < k2) return -1;
+        else if (k1 > k2) return +1;
+        else return 0;
+    });
+
+    var i, after = {};
+    for (i = 0; i < keys.length; i++) {
+        after[keys[i]] = obj[keys[i]];
+        delete obj[keys[i]];
+    }
+
+    for (i = 0; i < keys.length; i++) {
+        obj[keys[i]] = after[keys[i]];
+    }
+    return obj;
+}
